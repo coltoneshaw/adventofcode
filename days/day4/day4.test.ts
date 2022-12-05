89a72588d78e86928e04122d10ae51f2a6cef905
coltoneshaw <colton@theshaw.group>
2022-12-04 21:08:06 -0500

    day four both parts

---
+++ days/day4/day4.test.ts
@@ -0,0 +1,11 @@
+import { dayFourPartOne, dayFourPartTwo } from './day4';
+
+describe('Day four tests', () => {
+  test('Part one', () => {
+    expect(dayFourPartOne()).toBe(509);
+  });
+
+  test('Part two', () => {
+    expect(dayFourPartTwo()).toBe(0);
+  });
+});
---
+++ days/day4/day4.ts
@@ -0,0 +1,114 @@
+import path from 'path';
+import { syncReadFile } from '../helpers/file';
+
+const dayFourPartOne = () => {
+  const cleaningPairs = syncReadFile(path.join(__dirname, 'input.txt'));
+
+  let duplicatePairs = 0;
+
+  // loop through each pair
+  // generate an array with the cleaning items
+  // loop through the first array and see if it contains the whole second array
+  // if so, increment the counter
+
+  cleaningPairs.forEach((pair) => {
+    const [first, second] = pair
+      .split(',')
+      .map((item) => item.trim())
+      .map((item) => item.split('-'));
+
+    if (
+      Number(first[0]) <= Number(second[0])
+      && Number(first[1]) >= Number(second[1])
+    ) {
+      duplicatePairs += 1;
+
+      // console.log({
+      //   log: 'triggered on one',
+      //   first,
+      //   second,
+      // });
+      return;
+    }
+
+    if (
+      Number(second[0]) <= Number(first[0])
+      && Number(second[1]) >= Number(first[1])
+    ) {
+      duplicatePairs += 1;
+      // console.log({
+      //   log: 'triggered on two',
+      //   first,
+      //   second,
+      //   secondOne: second[0],
+      //   firstOne: first[0],
+      //   2: second[0] <= first[0],
+      // });
+    }
+  });
+
+  return duplicatePairs;
+};
+
+const dayFourPartTwo = () => {
+  const cleaningPairs = syncReadFile(path.join(__dirname, 'input.txt'));
+
+  let duplicatePairs = 0;
+
+  // loop through each pair
+  // generate an array with the cleaning items
+  // loop through the first array and see if it contains the whole second array
+  // if so, increment the counter
+
+  cleaningPairs.forEach((pair) => {
+    const [first, second] = pair
+      .split(',')
+      .map((item) => item.trim())
+      .map((item) => item.split('-'));
+
+    // contains the other.
+    if (
+      (
+        Number(first[0]) <= Number(second[0])
+        && Number(first[1]) >= Number(second[1])
+      )
+        || (
+          Number(second[0]) <= Number(first[0])
+        && Number(second[1]) >= Number(first[1])
+        )
+    ) {
+      duplicatePairs += 1;
+      return;
+    }
+
+    if (
+      +first[0] <= +second[0]
+      && +first[1] >= +second[0]
+    ) {
+      duplicatePairs += 1;
+      return;
+    }
+
+    if (
+      +first[1] >= +second[1]
+      && +first[0] <= +second[0]
+    ) {
+      duplicatePairs += 1;
+      return;
+    }
+
+    if (+first[0] <= +second[1] && +first[1] >= +second[1]) {
+      duplicatePairs += 1;
+      return;
+    }
+
+    console.log({ first, second });
+  });
+
+  return duplicatePairs;
+};
+
+export {
+  dayFourPartOne,
+  dayFourPartTwo,
+};
