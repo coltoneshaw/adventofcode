2f24cd3a13d98cc80bd848241228281fbbc3aca3
coltoneshaw <colton@theshaw.group>
2022-12-11 21:11:15 -0500

    Day 10 complete

--- days/day10/day10.ts
+++ days/day10/day10.ts
@@ -95,9 +95,65 @@ const dayTenPartOne = () => {
   return cycleValues.reduce((acc, value) => acc + value, 0);
 };
 
+const dayTenPartTwo = () => {
+  const cpuInstructions = syncReadFile(path.join(__dirname, 'input.txt'))
+    .map((line) => line.split(' '))
+    .map(([instruction, value]) => ({
+      instruction,
+      value: (value) ? +value : null,
+    }));
+
+  // this end value represents the X location of the row.
+  // each row is 39 pixels wide.
+
+  // this is the CRT tracker, 1 - 240
+  let crtPosition = 0;
+
+  // sprite is 3 pixels wide
+
+  const spriteRange = {
+    start: 0,
+    end: 2,
+  };
+  const screen: ('.' | '#')[][] = Array.from({ length: 6 }, () => Array.from({ length: 40 }, () => '.'));
+
+  const incrementCycle = () => {
+    // check the current
+
+    const currentXPosition = crtPosition % 40;
+    const samePosition = currentXPosition >= spriteRange.start && currentXPosition <= spriteRange.end;
+    screen[Math.floor(crtPosition / 40)][currentXPosition] = (samePosition) ? '#' : '.';
+    crtPosition += 1;
+  };
+
+  const addToEndValue = (value: number) => {
+    spriteRange.start += value;
+    spriteRange.end += value;
+  };
+
+  cpuInstructions.forEach(({ instruction, value }) => {
+    if (instruction === 'noop') {
+      doNoop(incrementCycle);
+      return;
+    }
+
+    if (instruction === 'addx') {
+      if (!value) {
+        throw Error('Value is required for addx instruction');
+      }
+
+      doAddx(incrementCycle, addToEndValue, value);
+    }
+  });
+
+  console.table(screen);
+  return screen;
+};
+
 // stop if you reach an edge
 // stop if you reach a tree the same height or taller
 
 export {
   dayTenPartOne,
+  dayTenPartTwo,
 };
